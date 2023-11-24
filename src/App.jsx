0977482d5de3f9b1c5a9b8acc0079a1a1de1f420
Guadalupe <guada@AirdeGuadalupe.home>
2023-11-24 12:52:36 +0100

    redesigning

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,228 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
 
-import About from "./Pages/About";
-import Experience from "./Pages/Projects";
-
-import Header from "./Components/Header";
-import Home from "./Pages/Home";
-import Hangman from "./Pages/Game";
 
 function App() {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/public/CV.pdf"; // Reemplaza con la ruta de tu CV
+    link.download = "GuadalupeDoudchitzkyCV.pdf"; // Nombre del archivo de descarga
+    link.click();
+  };
   return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/game" element={<Hangman />} />
-      </Routes>
+    <div className="porfolio_container">
+      <div className="aboutMe_container">
+        <h1>Guadalupe Doudchitzky Amadeo</h1>
+        <img
+          src="https://res.cloudinary.com/dpidlverd/image/upload/v1698675405/replica/WhatsApp_Image_2023-10-30_at_12.48.35_dvzhvy.jpg"
+          alt="avatar"
+        ></img>
+        <h4>Front-end developer and UX/UI designer</h4>
+        <div className="links_container">
+          <a href="https://www.linkedin.com/in/guadalupedoudchitzkyamadeo/">
+            <img
+              src="https://res.cloudinary.com/do7bnejaz/image/upload/v1682438843/assets/linkedin_ai0syv.png"
+              alt="LinkedIn"
+            />
+          </a>
+          <a href="https://github.com/Guadixx">
+            <img
+              src="https://res.cloudinary.com/dpidlverd/image/upload/v1698678003/replica/github_tw8doc.png"
+              alt="Git Hub"
+            />
+          </a>
+          <a href="tel:+34636191878">
+            <img
+              src="https://res.cloudinary.com/dpidlverd/image/upload/v1698679492/replica/free-apple-phone-2-493154_jdzaqn.webp"
+              alt="Telefono"
+            />
+          </a>
+          <a href="mailto:guadadoud@gmail.com">
+            <img
+              src="https://res.cloudinary.com/dpidlverd/image/upload/v1698679510/replica/gmail-icon-1024x1024-09wrt8am_v5rxzo.png"
+              alt="Gmail"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="aboutMe_content">
+        <div className="text_container">
+          <p>
+            {" "}
+            💡 Curiosity and observation have taken me to search constantly for
+            new challenges, experiences and learning. As a result, I find
+            myselfdeeply fullfilled with developing web pages and mobile
+            applications.
+          </p>
+          <p>
+            {" "}
+            🖇️ In particular, I am interested in pursuing a career as a Front
+            end Developer
+          </p>
+          <p>
+            {" "}
+            📱 I am dedicated to building intuitive and visually stunning
+            interfaces that make a lasting impression. My attention to detail
+            and strong problem-solving skills enable me to deliver high-quality
+            work, even when faced with complex challenges.
+          </p>
+          <p>
+            {" "}
+            🧉 On my free time you can find me doing anything art related like
+            music, painting or watching movies and last but not least
+            always drinking mate
+          </p>
+          <p> Hope we can meet soon.</p>
+          <button className="button_cv" onClick={handleDownload}>
+            Download CV
+          </button>
+        </div>
+        <div className="table_container">
+          <h1>Skills</h1>
+          <div className="table_content">
+            <table>
+              <thead>
+                <tr>
+                  <th>Language</th>
+                  <th>Framework</th>
+                  <th>Libraries</th>
+                  <th>Tools</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>JavaScript</td>
+                  <td>React</td>
+                  <td>Redux</td>
+                  <td>Bash</td>
+                </tr>
+                <tr>
+                  <td>TypeScript</td>
+                  <td>React Native</td>
+                  <td>React Router</td>
+                  <td>Git & Github</td>
+                </tr>
+                <tr>
+                  <td>HTML</td>
+                  <td>Node.js</td>
+                  <td>React Testing</td>
+                  <td>Chrome DevTools</td>
+                </tr>
+                <tr>
+                  <td>CSS</td>
+                  <td>Express</td>
+                  <td>AXIOS</td>
+                  <td>Postman</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>Bootstrap</td>
+                  <td>Framer Motion</td>
+                  <td>MongoDB</td>
+                </tr>
+
+                <tr>
+                  <td></td>
+                  <td>Wordpress</td>
+                  <td>Mongoose</td>
+                  <td>FIgma</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>Canva</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="experience_container">
+          <h1>Experience</h1>
+          <div className="experience_content">
+            <h4>May 2023 - August 2023</h4>
+            <div className="experience_box">
+              <h4>Front end Engineer Trainee - Chattigo</h4>
+              <p>
+                {" "}
+                Actively participating in identifying, resolving, and performing
+                code revisions and bug fixes to ensure optimal performance and
+                user experience.
+              </p>
+              <div className="technologies_div">
+                <p>Angular</p>
+                <p>Typescript</p>
+                <p>Mockoon</p>
+                <p>Homebrew</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="proyects_container">
+          <h1>Proyects</h1>
+          <a
+            href="https://github.com/Guadixx/TestBusters_Front/tree/main"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="proyects_content">
+              <img
+                className="images_tb"
+                src="https://res.cloudinary.com/dpidlverd/image/upload/v1686149427/Captura_de_Pantalla_2023-06-07_a_las_16.42.03_ln8k4s.png"
+                alt="TestBusters mockup"
+                loading="lazy"
+              />
+              <div className="proyects_box">
+                <h4>TestBusters</h4>
+                <p>
+                  Web application designed to make and create any type of test.
+                  All users have the possibility of creating their own tests on
+                  the subject of their choice.
+                </p>{" "}
+                <div className="technologies_div">
+                  <p>Javascript</p>
+                  <p>React</p>
+                  <p>Node</p>
+                  <p>Express</p>
+                  <p>MongoDB</p>
+                  <p>Mongoose</p>
+                </div>
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://github.com/kareenadg/MARVEL_GROUP_project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="proyects_content">
+              <img
+                className="images_tb"
+                src="https://res.cloudinary.com/dpidlverd/image/upload/v1686158661/marvelmaqueta_tld1uj.png"
+                alt="Marvel Mockup"
+                loading="lazy"
+              />
+              <div className="proyects_box">
+                <h4>Marvel Infinity Film</h4>
+                <p>
+                  Web application of the Marvel MCU movies, we provide an
+                  interactive platform for users to check updates, detailed
+                  information on the movies, and have access to a personalized
+                  watchlist and favorites list.
+                </p>{" "}
+                <div className="technologies_div">
+                  <p>Javascript</p>
+                  <p>React</p>
+                  <p>Node</p>
+                  <p>Express</p>
+                  <p>MongoDB</p>
+                  <p>Mongoose</p>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
