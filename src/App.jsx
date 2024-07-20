a9ff46c2b0c752ccdf0bfddabd88c3b6dba9b0b8
GuadaExyt <gdoud@exyt.es>
2024-07-20 21:58:19 +0200

    Portoflio 2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import "./App.css";
+import DownloadIcon from '@mui/icons-material/Download';
 
 
 function App() {
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/public/CV.pdf"; // Reemplaza con la ruta de tu CV
+    link.href = "/public/GuadalupeCV.pdf"; // Reemplaza con la ruta de tu CV
     link.download = "GuadalupeDoudchitzkyCV.pdf"; // Nombre del archivo de descarga
     link.click();
   };
   return (
     <div className="porfolio_container">
       <div className="aboutMe_container">
-        <h1>Guadalupe Doudchitzky Amadeo</h1>
         <img
-          src="https://res.cloudinary.com/dpidlverd/image/upload/v1698675405/replica/WhatsApp_Image_2023-10-30_at_12.48.35_dvzhvy.jpg"
+          src="https://res.cloudinary.com/dpidlverd/image/upload/v1721485415/replica/1688395371731_smytw3.jpg"
           alt="avatar"
         ></img>
+        <h1>Guadalupe Doudchitzky Amadeo</h1>
         <h4>Front-end developer and UX/UI designer</h4>
+        <h4 className="email_sender">
+        <a className="email_sender" href="mailto:guadadoud@gmail.com">guadadoud@gmail.com</a>
+        </h4>
+        <h4 className="phone"><a href="tel:+34636191878"></a>📞 636191878 </h4>
         <div className="links_container">
           <a href="https://www.linkedin.com/in/guadalupedoudchitzkyamadeo/">
             <img
@@ -30,18 +35,6 @@ function App() {
               alt="Git Hub"
             />
           </a>
-          <a href="tel:+34636191878">
-            <img
-              src="https://res.cloudinary.com/dpidlverd/image/upload/v1698679492/replica/free-apple-phone-2-493154_jdzaqn.webp"
-              alt="Telefono"
-            />
-          </a>
-          <a href="mailto:guadadoud@gmail.com">
-            <img
-              src="https://res.cloudinary.com/dpidlverd/image/upload/v1698679510/replica/gmail-icon-1024x1024-09wrt8am_v5rxzo.png"
-              alt="Gmail"
-            />
-          </a>
         </div>
       </div>
       <div className="aboutMe_content">
@@ -50,13 +43,12 @@ function App() {
             {" "}
             💡 Curiosity and observation have taken me to search constantly for
             new challenges, experiences and learning. As a result, I find
-            myselfdeeply fullfilled with developing web pages and mobile
+            myself deeply fullfilled with developing web pages and mobile
             applications.
           </p>
           <p>
             {" "}
-            🖇️ In particular, I am interested in pursuing a career as a Front
-            end Developer
+            🖇️ In particular, I am interested in pursuing a career as a Frontend Developer & UI designer
           </p>
           <p>
             {" "}
@@ -68,12 +60,12 @@ function App() {
           <p>
             {" "}
             🧉 On my free time you can find me doing anything art related like
-            music, painting or watching movies and last but not least
-            always drinking mate
+            music, painting or watching movies and cinema.
           </p>
           <p> Hope we can meet soon.</p>
-          <button className="button_cv" onClick={handleDownload}>
+          <button className="button_cv" onClick={handleDownload} >
             Download CV
+            <DownloadIcon className="icon-style" />
           </button>
         </div>
         <div className="table_container">
@@ -84,7 +76,6 @@ function App() {
                 <tr>
                   <th>Language</th>
                   <th>Framework</th>
-                  <th>Libraries</th>
                   <th>Tools</th>
                 </tr>
               </thead>
@@ -92,42 +83,35 @@ function App() {
                 <tr>
                   <td>JavaScript</td>
                   <td>React</td>
-                  <td>Redux</td>
-                  <td>Bash</td>
+                  <td>Docker</td>
                 </tr>
                 <tr>
                   <td>TypeScript</td>
-                  <td>React Native</td>
-                  <td>React Router</td>
+                  <td>Angular (11,12,16)</td>
                   <td>Git & Github</td>
                 </tr>
                 <tr>
                   <td>HTML</td>
                   <td>Node.js</td>
-                  <td>React Testing</td>
-                  <td>Chrome DevTools</td>
+                  <td>SASS</td>
                 </tr>
                 <tr>
                   <td>CSS</td>
                   <td>Express</td>
-                  <td>AXIOS</td>
                   <td>Postman</td>
                 </tr>
                 <tr>
-                  <td></td>
+                  <td>PYTHON</td>
                   <td>Bootstrap</td>
-                  <td>Framer Motion</td>
                   <td>MongoDB</td>
                 </tr>
 
                 <tr>
                   <td></td>
                   <td>Wordpress</td>
-                  <td>Mongoose</td>
                   <td>FIgma</td>
                 </tr>
                 <tr>
-                  <td></td>
                   <td></td>
                   <td></td>
                   <td>Canva</td>
@@ -138,6 +122,27 @@ function App() {
         </div>
         <div className="experience_container">
           <h1>Experience</h1>
+          <div className="experience_content">
+            <h4>Dec 2023 - Current</h4>
+            <div className="experience_box">
+              <h4>Frontend developer & UI designer</h4>
+              <p>
+                {" "}
+                Currently, involved in developing, designing and maintaining applications using Angular 11, 12, 16, Sass, CSS, HTML and Typescript. Collaborating with backend developers. Developing applications focused on scanning and validating QR codes. Engaged in continuous learning of technologies.   
+              </p>
+              <div className="technologies_div">
+                <p>Angular</p>
+                <p>Typescript</p>
+                <p>Javascript</p>
+                <p>Figma</p>
+                <p>Docker</p>
+                <p>Postman</p>
+                <p>Python</p>
+                <p>Google Cloud Datastore</p>
+                <p>Google Cloud Run</p>
+              </div>
+            </div>
+          </div>
           <div className="experience_content">
             <h4>May 2023 - August 2023</h4>
             <div className="experience_box">
@@ -159,6 +164,33 @@ function App() {
         </div>
         <div className="proyects_container">
           <h1>Proyects</h1>
+          <a
+          
+          >
+          <div className="proyects_content">
+              <img
+                className="images_tb"
+                src="https://res.cloudinary.com/dpidlverd/image/upload/v1721489945/replica/Captura_de_pantalla_2024-07-20_a_las_17.39.02_vamjh9.png"
+                alt="TestBusters mockup"
+                loading="lazy"
+              />
+              <div className="proyects_box">
+                <h4>Cheapsify (Coming soon)</h4>
+                <p>
+                Cheapsify is your ultimate grocery shopping companion. This app compares supermarket prices in your vicinity, helping you find the cheapest options effortlessly. By analyzing real-time data from nearby stores, Cheapsify ensures you get the best deals and save money on your groceries. 
+                </p>{" "}
+                <div className="technologies_div">
+                  <p>Figma</p>
+                  <p>Javascript</p>
+                  <p>React</p>
+                  <p>Node</p>
+                  <p>Express</p>
+                  <p>MongoDB</p>
+                  <p>Mongoose</p>
+                </div>
+              </div>
+            </div>
+            </a>
           <a
             href="https://github.com/Guadixx/TestBusters_Front/tree/main"
             target="_blank"
